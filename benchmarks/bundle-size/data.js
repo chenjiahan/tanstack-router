5442ec427e4ab0072b8496fcb1f8ef6a4dc12ea6
github-action-benchmark <github@users.noreply.github.com>
2026-04-11 21:34:58 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for 0e2c9003c18ae07c09969189c028f277ea562a7a

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775931479320,
+  "lastUpdate": 1775943298736,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -10441,6 +10441,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/812792fbda3caf97b300770855cf5641252f413b"
         },
         "date": 1775931477754,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 89436,
+            "unit": "bytes",
+            "extra": "raw=281229; brotli=77707"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92789,
+            "unit": "bytes",
+            "extra": "raw=292568; brotli=80613"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 36382,
+            "unit": "bytes",
+            "extra": "raw=109199; brotli=32727"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 40948,
+            "unit": "bytes",
+            "extra": "raw=123013; brotli=36740"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 54591,
+            "unit": "bytes",
+            "extra": "raw=155694; brotli=49057"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 59602,
+            "unit": "bytes",
+            "extra": "raw=171485; brotli=53362"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 104188,
+            "unit": "bytes",
+            "extra": "raw=330099; brotli=90072"
+          },
+          {
+            "name": "react-start.full",
+            "value": 107714,
+            "unit": "bytes",
+            "extra": "raw=340678; brotli=93047"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 50722,
+            "unit": "bytes",
+            "extra": "raw=156107; brotli=44717"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 56364,
+            "unit": "bytes",
+            "extra": "raw=172699; brotli=49619"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "74932975+birkskyum@users.noreply.github.com",
+            "name": "Birk Skyum",
+            "username": "birkskyum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0e2c9003c18ae07c09969189c028f277ea562a7a",
+          "message": "chore: bump to h3 v2 rc.20 (#7140)",
+          "timestamp": "2026-04-11T23:32:39+02:00",
+          "tree_id": "ffb7b903125ab6a4b8b99cba39e05051c80fa391",
+          "url": "https://github.com/TanStack/router/commit/0e2c9003c18ae07c09969189c028f277ea562a7a"
+        },
+        "date": 1775943297688,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
